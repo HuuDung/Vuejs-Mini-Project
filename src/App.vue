5b4fcb575d3dbc2c777ff62af1105216608cd580
dungnh <hu_nguyen@trancom.co.jp>
2021-04-09 17:24:17 +0900

    modal

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,9 @@
   <div id="app">
     <header-nav></header-nav>
     <router-view />
-    <div class="container">
-      <ckeditor
-        :editor="editor"
-        v-model="editorData"
-        :config="editorConfig"
-        tag-name="textarea"
-      ></ckeditor>
-      <template>
-        <div v-html="editorData"></div>
-      </template>
-    </div>
   </div>
 </template>
 <script>
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import HeaderNav from "./components/Layout/Header";
 
 export default {
@@ -25,14 +13,7 @@ export default {
     HeaderNav,
   },
   data() {
-    return {
-      editor: ClassicEditor,
-      editorData: "",
-      editorConfig: {
-        // The configuration of the editor.
-        toolbar: ["bold", "italic", "|","bulletedList", "numberedList",""],
-      },
-    };
+    return {};
   },
   methods: {},
 };
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,78 @@
 <template>
-  <div class="home text-center">
+  <div class="container">
     <h1>HomePage</h1>
+    <div class=" text-center">
+      <div>
+        <b-button @click="$bvModal.show('modal-scoped')" variant="info"
+          >Open Modal</b-button
+        >
+        <b-modal
+          id="modal-scoped"
+          size="xl"
+          header-bg-variant="info"
+          header-text-variant="light"
+        >
+          <template #modal-header>
+            <h3>レシピ作成</h3>
+          </template>
+
+          <template #default>
+            <div class="form-group">
+              <p>材料</p>
+              <ckeditor
+                :editor="editor"
+                v-model="recipe.spices"
+                :config="editorConfig"
+                tag-name="textarea"
+              ></ckeditor>
+            </div>
+            <div class="form-group">
+              <p>手順</p>
+              <ckeditor
+                :editor="editor"
+                v-model="recipe.process"
+                :config="editorConfig"
+                tag-name="textarea"
+              ></ckeditor>
+            </div>
+          </template>
+
+          <template #modal-footer="{ ok, cancel }">
+            <!-- Emulate built in modal footer ok and cancel button actions -->
+            <b-button size="" variant="success" @click="ok()"> 追加 </b-button>
+            <b-button size="" variant="danger" @click="cancel()">
+              キャンセル
+            </b-button>
+          </template>
+        </b-modal>
+      </div>
+    </div>
+    <div>
+      <template>
+        <p>材料</p>
+        <div v-html="recipe.spices"></div>
+        <p>手順</p>
+        <div v-html="recipe.process"></div>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 export default {
   name: "Home",
   data() {
     return {
+      editor: ClassicEditor,
+      editorConfig: {
+        // The configuration of the editor.
+        toolbar: ["bold", "italic", "|", "bulletedList", "numberedList"],
+      },
+      recipe: {
+        process: "",
+        spices: "",
+      },
       logged: false,
     };
   },
