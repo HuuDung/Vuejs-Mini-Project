603ddd18582947772efee8ef8b8ec4fde7aca777
dungnh <hu_nguyen@trancom.co.jp>
2021-04-12 15:32:37 +0900

    update

--- src/components/Recipe/ItemCard.vue
+++ src/components/Recipe/ItemCard.vue
@@ -1,48 +1,44 @@
 <template>
   <div class="mb-4">
-    <b-row>
-      <b-col
-        ><b-card
-          img-src="https://placekitten.com/300/300"
-          img-alt="Card recipe item image"
-          img-left
-          class="mb-1"
-          border-variant="info"
+    <b-card
+      img-src="https://placekitten.com/300/300"
+      img-alt="Card recipe item image"
+      img-left
+      class="mb-1"
+      border-variant="info"
+    >
+      <b-card-header>
+        <h4>{{ recipe.title || "" }}</h4>
+      </b-card-header>
+      <b-card-text class="mt-4">
+        <div>
+          <label for="" class="font-weight-bold">料理名:</label>
+          <label for="">{{ recipe.name }}</label>
+        </div>
+        <div>
+          <label for="" class="font-weight-bold">材料:</label>
+          <label for="">{{ recipe.foods }}</label>
+        </div>
+        <div>
+          <label for="" class="font-weight-bold">調味料:</label>
+          <label for="">{{ recipe.spices }}</label>
+        </div>
+        <div>
+          <label for="" class="font-weight-bold">何人前:</label>
+          <label for="">{{ recipe.servings }}</label>
+        </div>
+        <div>
+          <label for="" class="font-weight-bold">所要時間:</label>
+          <label for="">{{ recipe.time }}</label>
+        </div>
+      </b-card-text>
+      <b-card-footer class="text-center">
+        <b-button variant="outline-primary" class="mr-4" size="xl"
+          >編集</b-button
         >
-          <b-card-header>
-            <h4>{{ recipe.title || "" }}</h4>
-          </b-card-header>
-          <b-card-text class="mt-4">
-            <div>
-              <label for="" class="font-weight-bold">料理名:</label>
-              <label for="">{{ recipe.name }}</label>
-            </div>
-            <div>
-              <label for="" class="font-weight-bold">材料:</label>
-              <label for="">{{ recipe.foods }}</label>
-            </div>
-            <div>
-              <label for="" class="font-weight-bold">調味料:</label>
-              <label for="">{{ recipe.spices }}</label>
-            </div>
-            <div>
-              <label for="" class="font-weight-bold">何人前:</label>
-              <label for="">{{ recipe.servings }}</label>
-            </div>
-            <div>
-              <label for="" class="font-weight-bold">所要時間:</label>
-              <label for="">{{ recipe.time }}</label>
-            </div>
-          </b-card-text>
-          <b-card-footer class="text-center">
-            <b-button variant="outline-primary" class="mr-4" size="xl"
-              >編集</b-button
-            >
-            <b-button variant="outline-danger">削除</b-button>
-          </b-card-footer>
-        </b-card>
-      </b-col>
-    </b-row>
+        <b-button variant="outline-danger">削除</b-button>
+      </b-card-footer>
+    </b-card>
   </div>
 </template>
 <script>
@@ -68,5 +64,8 @@ export default {
 }
 .card img {
   padding: 10px;
+}
+.even {
+    padding-left: 10px;
 }
 </style>
--- src/components/Recipe/ItemList.vue
+++ src/components/Recipe/ItemList.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <item-card
-      v-for="recipe in recipes"
-      :recipe="recipe"
-      :key="recipe.id"
-    ></item-card>
+    <b-card-group>
+      <item-card
+        v-for="recipe in recipes"
+        :recipe="recipe"
+        :key="recipe.id"
+      ></item-card>
+    </b-card-group>
   </div>
 </template>
 <script>
@@ -30,6 +32,15 @@ export default {
           title: "タイトル",
           foods: "材料",
           name: "料理名",
+          spices: "調味料",
+          servings: "何人前",
+          time: "所要時間",
+        },
+        {
+          id: 3,
+          title: "タイトル",
+          name: "料理名",
+          foods: "材料",
           spices: "調味料",
           servings: "何人前",
           time: "所要時間",
